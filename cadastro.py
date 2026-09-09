e491107fd527d963b93f0f0b818fa2b11977034c
lhetx <henrique.takahashia@gmail.com>
2026-09-09 13:00:01 -0300

    feat: adiciona captura do preço do produto

--- cadastro.py
+++ cadastro.py
@@ -1,9 +1,13 @@
 def obter_nome_produto():
     return input("Digite o nome do produto: ")
 
+def obter_preco_produto():
+    return float(input("Digite o preço: "))
+
 def main():
     nome = obter_nome_produto()
-    print(f"Produto cadastrado: {nome}")
+    preco = obter_preco_produto()
+    print(f"Produto: {nome} - Preço: R$ {preco:.2f}")
 
 if __name__ == "__main__":
     main()
